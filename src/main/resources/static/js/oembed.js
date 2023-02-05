70a36b374f5e696ee703d9b1f280b5de8edb6c73
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-02-05 20:58:02 +0900

    json 형태로 받아온 값을 key와 value로 html 처리

--- src/main/resources/static/js/oembed.js
+++ src/main/resources/static/js/oembed.js
@@ -14,76 +14,60 @@ function search(){
     })
 }
 function resultHtml(data) {
+
+    var html ="<table style='width:1000px;'>"
+    html += "<div class='div_4'>"
     if (data.error==="error"){
         alert(data.msg)
+        return;
     }
     else if(data.access_url=="youtube") {
-        var html ="<table style='width:1000px;'>"
-        html += "<div class='div_4'>"
-        html += "<div> " + "<span style='background-color:#f4f4f4 '>title</span><span style='font-weight: bold; background-color:#f4f4f4 '>" + data.title + "</span></div>"
-        html +=  "<div>" + "<span>type</span><span>" + data.type + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4 '>version</span><span style='background-color:#f4f4f4 '>" + data.version + "</span></div>"
-        html +=  "<div>" + "<span>provider_name</span><span>" + data.provider_name + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4'>provider_url</span><span style='background-color:#f4f4f4'>" +"<a href="+ data.provider_url + ">"+data.provider_url+"</a></span></div>"
-        html +=  "<div>" + "<span>author_name</span><span>" + data.author_name + "</span></div>"
-        html+=  "<div>" + "<span style='background-color:#f4f4f4'>author_url</span><span style='background-color:#f4f4f4'>" +"<a href="+ data.author_url + ">"+data.author_url+"</a></span></div>"
-        html+= "<div>" + "<span style='height:200px'>html<br/>(" + data.width + "/" + data.height + ")</span>" +
-            "<span style='height:200px;'><xmp>" + data.html + "</xmp>"+ data.html + "</span></div>";
-        html +=  "<div>" + "<span style='background-color:#f4f4f4'>width</span><span style='background-color:#f4f4f4'>" + data.width + "</span></div>"
-        html +=  "<div>" + "<span>height</span><span>" + data.height + "</span></div>"
-        html+="<div>" + "<span style='height: 500px; background-color: #f4f4f4'>thumbnail_url<br/>(" + data.thumbnail_width + "/" + data.thumbnail_height + ")</span><span style='height: 500px; background-color: #f4f4f4'><a href='" +data.thumbnail_url+"'>" + data.thumbnail_url + "</a><br/><br/><img src='"+data.thumbnail_url+"'/></span></div>";
-        html +=  "<div>" + "<span>thumbnail_width</span><span>" + data.thumbnail_width + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4 '>thumbnail_height</span><span style='background-color: #f4f4f4'>" + data.thumbnail_height + "</span></div>"
-        html+="</div>"
-        $("#content").empty();
-        $("#content").append(html);
+        $.each(data, function(key, value){
+            if (key === "html") {
+                html+= "<div>" + "<span style='height:200px'>html<br/>(" + data.width + "/" + data.height + ")</span>" +
+                    "<span style='height:200px;'><xmp>" + data.html + "</xmp>"+ data.html + "</span></div>";
+            } else if (key === "thumbnail_url"){
+                html+="<div>" + "<span style='height: 500px;'>thumbnail_url<br/>(" + data.thumbnail_width + "/" + data.thumbnail_height + ")</span><span style='height: 500px;'><a href='" +data.thumbnail_url+"'>" + data.thumbnail_url + "</a><br/><br/><img src='"+data.thumbnail_url+"'/></span></div>";
+            } else if (key === "author_url" || key === "provider_url"){
+                //
+                html +=  "<div>" + "<span>" + key + "</span><span>" +"<a href="+ value + ">"+value+"</a></span></div>"
+            }else {
+                html += "<div> " + "<span>"+ key +"</span><span style='font-weight: bold;'>" + value + "</span></div>"
+            }
+        });
     }
     else if(data.access_url=="twitter"){
-        var html ="<table style='width:1000px;'>"
-        html += "<div class='div_4'>"
-        html +=  "<div>" + "<span>type</span><span>" + data.type + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4 '>version</span><span style='background-color:#f4f4f4 '>" + data.version + "</span></div>"
-        html +=  "<div>" + "<span>provider_name</span><span>" + data.provider_name + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4'>provider_url</span><span style='background-color:#f4f4f4'>" +"<a href="+ data.provider_url + ">"+data.provider_url+"</a></span></div>"
-        html +=  "<div>" + "<span>author_name</span><span>" + data.author_name + "</span></div>"
-        html+=  "<div>" + "<span style='background-color:#f4f4f4'>author_url</span><span style='background-color:#f4f4f4'>" +"<a href="+ data.author_url + ">"+data.author_url+"</a></span></div>"
-        html+= "<div>" + "<span style='height:1000px'>html<br/>(" + data.width + "/" + data.height + ")</span>" +
-            "<span style='height:1000px;'><xmp style='display: inline-table;'>" + data.html + "</xmp>"+ data.html + "</span></div>";
-        html +=  "<div>" + "<span style='background-color:#f4f4f4'>width</span><span style='background-color:#f4f4f4'>" + data.width + "</span></div>"
-        html +=  "<div>" + "<span>height</span><span>" + data.height + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4 '>cache_age</span><span style='background-color:#f4f4f4'>" + data.cache_age + "</span></div>"
-        html +=  "<div>" + "<span>description</span><span>" + data.description + "</span></div>"
-        html+=  "<div>" + "<span style='background-color:#f4f4f4'>url</span><span style='background-color:#f4f4f4'>" +"<a href="+ data.url + ">"+data.url+"</a></span></div>"
-        html+="</div>"
-        $("#content").empty();
-        $("#content").append(html);
+        $.each(data, function(key, value){
+            if (key === "html") {
+                html+= "<div>" + "<span style='height:1500px'>html<br/>(" + data.width + "/" + data.height + ")</span>" +
+                    "<span style='height:1500px;'><xmp style='display: inline-table;'>" + data.html + "</xmp>"+ data.html + "</span></div>";
+            } else if (key === "thumbnail_url"){
+                html+="<div>" + "<span style='height: "+data.thumbnail_height+"px; margin-bottom: 30'>thumbnail_url<br/>(" + data.thumbnail_width + "/" + data.thumbnail_height + ")</span><span style='height: "+data.thumbnail_height+"px; margin-bottom: 30'><a href='" +data.thumbnail_url+"'>" + data.thumbnail_url + "</a><br/><br/><img src='"+data.thumbnail_url+"'/></span></div>";
+            } else if (key === "author_url" || key === "provider_url" || key === "url"){
+                //
+                html +=  "<div>" + "<span>" + key + "</span><span>" +"<a href="+ value + ">"+value+"</a></span></div>"
+            }else {
+                html += "<div> " + "<span>"+ key +"</span><span style='font-weight: bold;'>" + value + "</span></div>"
+            }
+        });
     }
     else if(data.access_url=="vimeo"){
-        var html ="<table style='width:1000px;'>"
-        html += "<div class='div_4'>"
-        html += "<div> " + "<span style='background-color:#f4f4f4 '>title</span><span style='font-weight: bold; background-color:#f4f4f4 '>" + data.title + "</span></div>"
-        html +=  "<div>" + "<span>type</span><span>" + data.type + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4 '>version</span><span style='background-color:#f4f4f4 '>" + data.version + "</span></div>"
-        html +=  "<div>" + "<span>provider_name</span><span>" + data.provider_name + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4'>provider_url</span><span style='background-color:#f4f4f4'>" +"<a href="+ data.provider_url + ">"+data.provider_url+"</a></span></div>"
-        html +=  "<div>" + "<span>author_name</span><span>" + data.author_name + "</span></div>"
-        html+=  "<div>" + "<span style='background-color:#f4f4f4'>author_url</span><span style='background-color:#f4f4f4'>" +"<a href="+ data.author_url + ">"+data.author_url+"</a></span></div>"
-        html+= "<div>" + "<span style='height:700px'>html<br/>(" + data.width + "/" + data.height + ")</span>" +
-            "<span style='height:700px;'><xmp>" + data.html + "</xmp>"+ data.html + "</span></div>";
-        html +=  "<div>" + "<span style='background-color:#f4f4f4'>width</span><span style='background-color:#f4f4f4'>" + data.width + "</span></div>"
-        html +=  "<div>" + "<span>height</span><span>" + data.height + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4 '>duration</span><span style='background-color:#f4f4f4'>" + data.duration + "</span></div>"
-        html +=  "<div>" + "<span>description</span><span>" + data.description + "</span></div>"
-        html+="<div>" + "<span style='height: 700px; background-color: #f4f4f4'>thumbnail_url<br/>(" + data.thumbnail_width + "/" + data.thumbnail_height + ")</span><span style='height: 700px; background-color: #f4f4f4'><a href='" +data.thumbnail_url+"'>" + data.thumbnail_url + "</a><br/><br/><img src='"+data.thumbnail_url+"'/></span></div>";
-        html +=  "<div>" + "<span>thumbnail_width</span><span>" + data.thumbnail_width + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4 '>thumbnail_height</span><span style='background-color: #f4f4f4'>" + data.thumbnail_height + "</span></div>"
-        html +=  "<div>" + "<span style='height: 100px;'>thumbnail_url_with_<br/>play_button</span><span style='height: 100px;'>" +"<a href="+ data.thumbnail_url_with_play_button + ">"+data.thumbnail_url_with_play_button+"</a></span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4'>upload_date</span><span style='background-color:#f4f4f4 '>" + data.upload_date + "</span></div>"
-        html +=  "<div>" + "<span>video_id</span><span>" + data.video_id + "</span></div>"
-        html +=  "<div>" + "<span style='background-color:#f4f4f4 '>uri</span><span style='background-color:#f4f4f4'>" + data.uri + "</span></div>"
-        html+="</div>"
-        $("#content").empty();
-        $("#content").append(html);
+        $.each(data, function(key, value){
+            if (key === "html") {
+                html+= "<div>" + "<span style='height:"+data.height+"px; margin-bottom: 75'>html<br/>(" + data.width + "/" + data.height + ")</span>" +
+                    "<span style='height:"+data.height+"px; margin-bottom: 75'><xmp>" + data.html + "</xmp>"+ data.html + "</span></div>";
+            } else if (key === "thumbnail_url"){
+                html+="<div>" + "<span style='height: 500px;'>thumbnail_url<br/>(" + data.thumbnail_width + "/" + data.thumbnail_height + ")</span><span style='height: 500px;'><a href='" +data.thumbnail_url+"'>" + data.thumbnail_url + "</a><br/><br/><img src='"+data.thumbnail_url+"'/></span></div>";
+            } else if (key === "author_url" || key === "provider_url"){
+                html +=  "<div>" + "<span>" + key + "</span><span>" +"<a href="+ value + ">"+value+"</a></span></div>"
+            } else if (key === "thumbnail_url_with_play_button"){
+                html +=  "<div>" + "<span style='height: 100px;'>thumbnail_url_with_<br/>play_button</span><span style='height: 100px;'>" +"<a href="+ data.thumbnail_url_with_play_button + ">"+data.thumbnail_url_with_play_button+"</a></span></div>"
+            } else {
+                html += "<div> " + "<span>"+ key +"</span><span style='font-weight: bold;'>" + value + "</span></div>"
+            }
+        });
     }
-
+    html+="</div>"
+    $("#content").empty();
+    $("#content").append(html);
 }
